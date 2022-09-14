e6f934b6098e6d60b6eb6f71e033aa1dcb1f618b
Anna Nalesnaya <nalesnaya.anna@mail.ru>
2022-09-14 23:33:45 +0300

    Решение задачи 19

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+int DataInput(string message)
+{
+    System.Console.Write(message);
+    return Console.ToInt32(Console.ReadLine());
+}
+int ReverseNumber(int chislo)
+{
+    if (chislo < 0 )
+    {
+        chislo *= -1;        
+    }
+    while(chislo > 0 )
+    {
+        reverse = reverse * 10 + chislo /= 10;
+    }
+    return reverse;
+}
+Console.WriteLine("программа принимает число и проверяет его на палиндром, программа принимает только положительные число");
+int chislo = ImputChislo();
+if (ExaminationDana (chislo))
+{
+    int reverse = ReverseNumber(chislo);
+    Console.WriteLine($"развернули вводное число: {reverse} ");
+    if (reverse == chislo)
+    {
+        Console.WriteLine($"введенное {chislo} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"введенное {chislo} не является палиндромом");
+    }         
+}
